e58d57a9f7898fc14b76af162e6dd8a52b9d808d
Quoc viet <vietprovip2k3@gmail.com>
2025-05-25 21:30:55 +0700

    fix 2

--- src/components/GameRoom.js
+++ src/components/GameRoom.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useParams, useNavigate, useLocation } from "react-router-dom";
 import "../assets/styles/GameRoom.css";
 import socket from "../socket";
