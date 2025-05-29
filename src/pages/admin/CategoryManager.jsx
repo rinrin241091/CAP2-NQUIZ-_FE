14ad2219355e380af493d1f90635c91b48d5c1a2
zit12 <101919931+zit12@users.noreply.github.com>
2025-05-29 13:44:56 +0700

    Update CategoryManager.jsx

--- src/pages/admin/CategoryManager.jsx
+++ src/pages/admin/CategoryManager.jsx
@@ -22,7 +22,7 @@ const CategoryManager = ({ open, onClose }) => {
 
   const fetchCategories = async () => {
     try {
-      const res = await axiosInstance.get('/categories');
+      const res = await axiosInstance.get('/categoriess');
       setCategories(res.data?.data || []);
     } catch (err) {
       console.error('Failed to fetch categories:', err);
